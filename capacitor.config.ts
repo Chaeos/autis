40893dd6e286bdaf4ac1b210f82ebee2390c8bdb
salva <svinyo@mtcplan.com>
2023-03-12 13:44:16 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Autis',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
